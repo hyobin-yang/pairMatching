6f130f60af991057b3c79c91ff110571f6a73db6
양효빈 <hyobin3026@naver.com>
2024-11-25 14:25:02 +0900

    feat(Answer): 입력받을 대답에 대한 enum 클래스 구현

---
+++ src/main/java/pairmatching/model/Answer.java
@@ -0,0 +1,11 @@
+package pairmatching.model;
+
+public enum Answer {
+    YES("네"), NO("아니오");
+
+    private final String answer;
+
+    Answer(String answer) {
+        this.answer = answer;
+    }
+}
