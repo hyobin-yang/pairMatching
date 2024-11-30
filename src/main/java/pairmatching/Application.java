d2a684bb46ae94f0a3ddf85e8e1cc3b7cb96259f
양효빈 <hyobin3026@naver.com>
2024-11-30 23:40:57 +0900

    refactor(Application): 파일 읽는 로직 삭제

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,20 +1,13 @@
 package pairmatching;
 
 import pairmatching.controller.PairMatchingController;
-import pairmatching.io.DataProvider;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
 public class Application {
-    private static final String BACKEND_CREW_FILE = "src/main/resources/backend-crew.md";
-    private static final String FRONTEND_CREW_FILE = "src/main/resources/frontend-crew.md";
 
     public static void main(String[] args) {
-        DataProvider dataProvider = new DataProvider();
-
-        PairMatchingController controller = new PairMatchingController(new InputView(), new OutputView(),
-                dataProvider.provideCrewData(BACKEND_CREW_FILE), dataProvider.provideCrewData(FRONTEND_CREW_FILE));
-
+        PairMatchingController controller = new PairMatchingController(new InputView(), new OutputView());
         controller.run();
     }
 }
