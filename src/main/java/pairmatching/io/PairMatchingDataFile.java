e826db01719e966b8bd9001521ac87611f5732a3
양효빈 <hyobin3026@naver.com>
2024-11-30 23:40:14 +0900

    feat(PairMatchingDataFile): 페어매칭을 위해 제공되는 파일 경로 상수화

---
+++ src/main/java/pairmatching/io/PairMatchingDataFile.java
@@ -0,0 +1,9 @@
+package pairmatching.io;
+
+public class PairMatchingDataFile {
+    public static final String BACKEND_CREW_FILE = "src/main/resources/backend-crew.md";
+    public static final String FRONTEND_CREW_FILE = "src/main/resources/frontend-crew.md";
+
+    private PairMatchingDataFile(){
+    }
+}
